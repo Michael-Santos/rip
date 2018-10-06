46dd061b675beb57d7bea0c94439228b46441edc
Michael Santos <michael-santos@outlook.com.br>
2018-10-05 21:17:36 -0300

    Adicionados os endereços de cada interface de cada Roteador

---
+++ rip.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+
+
+# Define o endereço de cada interface de cada um dos roteadores
+NODE0_INTERFACES_ADD = [ '192.168.1.100', '192.168.2.100', '192.168.3.100' ]
+NODE1_INTERFACES_ADD = [ '192.168.1.200', '192.168.5.100' ]
+NODE2_INTERFACES_ADD = [ '192.168.3.200', '192.168.4.100' ]
+NODE3_INTERFACES_ADD = [ '192.168.5.200', '192.168.2.200', '192.168.4.200' ]
+
+#def receiver():
+
+
+#def serder():
+
+lista = []
+
+
+
+
+
+
+
+
+
+
+#Cria o socket UDP
